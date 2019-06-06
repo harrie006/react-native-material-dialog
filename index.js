50caa6dc4062e93b75429052c14b6a02d023b3b7
harish lalwani <harrie006@gmail.com>
2019-06-06 19:14:00 +0530

    InputBoxMaterialDialog added to index.js
    InputBoxMaterialDialog

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import MaterialDialog from './src/MaterialDialog';
 import MultiPickerMaterialDialog from './src/MultiPickerMaterialDialog';
 import SinglePickerMaterialDialog from './src/SinglePickerMaterialDialog';
+import InputBoxMaterialDialog from './src/InputBoxMaterialDialog';
 
 const Dialogs = {
   MaterialDialog,
   MultiPickerMaterialDialog,
   SinglePickerMaterialDialog,
+  InputBoxMaterialDialog,
 };
 
 module.exports = Dialogs;
